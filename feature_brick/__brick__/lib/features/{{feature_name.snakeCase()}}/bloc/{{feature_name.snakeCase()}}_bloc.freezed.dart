6ef04de2f4efe35254cddcee1a9a793ef433698c
Rovshen Meredow <luxxicm@gmail.com>
2024-12-31 04:52:02 +0500

    freezed bloc added

---
+++ feature_brick/__brick__/lib/features/{{feature_name.snakeCase()}}/bloc/{{feature_name.snakeCase()}}_bloc.freezed.dart
@@ -0,0 +1,327 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of '{{feature_name.snakeCase()}}_bloc.freezed.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _${{feature_name.pascalCase()}}Event {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() init,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? init,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? init,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Init value) init,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Init value)? init,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Init value)? init,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class ${{feature_name.pascalCase()}}EventCopyWith<$Res> {
+  factory ${{feature_name.pascalCase()}}EventCopyWith(
+          {{feature_name.pascalCase()}}Event value, $Res Function({{feature_name.pascalCase()}}Event) then) =
+      _${{feature_name.pascalCase()}}EventCopyWithImpl<$Res, {{feature_name.pascalCase()}}Event>;
+}
+
+/// @nodoc
+class _${{feature_name.pascalCase()}}EventCopyWithImpl<$Res, $Val extends {{feature_name.pascalCase()}}Event>
+    implements ${{feature_name.pascalCase()}}EventCopyWith<$Res> {
+  _${{feature_name.pascalCase()}}EventCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of {{feature_name.pascalCase()}}Event
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$InitImplCopyWith<$Res> {
+  factory _$$InitImplCopyWith(
+          _$InitImpl value, $Res Function(_$InitImpl) then) =
+      __$$InitImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$InitImplCopyWithImpl<$Res>
+    extends _${{feature_name.pascalCase()}}EventCopyWithImpl<$Res, _$InitImpl>
+    implements _$$InitImplCopyWith<$Res> {
+  __$$InitImplCopyWithImpl(_$InitImpl _value, $Res Function(_$InitImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of {{feature_name.pascalCase()}}Event
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$InitImpl implements _Init {
+  const _$InitImpl();
+
+  @override
+  String toString() {
+    return '{{feature_name.pascalCase()}}Event.init()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$InitImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() init,
+  }) {
+    return init();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? init,
+  }) {
+    return init?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? init,
+    required TResult orElse(),
+  }) {
+    if (init != null) {
+      return init();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Init value) init,
+  }) {
+    return init(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Init value)? init,
+  }) {
+    return init?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Init value)? init,
+    required TResult orElse(),
+  }) {
+    if (init != null) {
+      return init(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Init implements {{feature_name.pascalCase()}}Event {
+  const factory _Init() = _$InitImpl;
+}
+
+/// @nodoc
+mixin _${{feature_name.pascalCase()}}State {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function() $default,
+  ) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function()? $default,
+  ) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function()? $default, {
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_{{feature_name.pascalCase()}}State value) $default,
+  ) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_{{feature_name.pascalCase()}}State value)? $default,
+  ) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_{{feature_name.pascalCase()}}State value)? $default, {
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class ${{feature_name.pascalCase()}}StateCopyWith<$Res> {
+  factory ${{feature_name.pascalCase()}}StateCopyWith(
+          {{feature_name.pascalCase()}}State value, $Res Function({{feature_name.pascalCase()}}State) then) =
+      _${{feature_name.pascalCase()}}StateCopyWithImpl<$Res, {{feature_name.pascalCase()}}State>;
+}
+
+/// @nodoc
+class _${{feature_name.pascalCase()}}StateCopyWithImpl<$Res, $Val extends {{feature_name.pascalCase()}}State>
+    implements ${{feature_name.pascalCase()}}StateCopyWith<$Res> {
+  _${{feature_name.pascalCase()}}StateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of {{feature_name.pascalCase()}}State
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$${{feature_name.pascalCase()}}StateImplCopyWith<$Res> {
+  factory _$${{feature_name.pascalCase()}}StateImplCopyWith(
+          _${{feature_name.pascalCase()}}StateImpl value, $Res Function(_${{feature_name.pascalCase()}}StateImpl) then) =
+      __$${{feature_name.pascalCase()}}StateImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$${{feature_name.pascalCase()}}StateImplCopyWithImpl<$Res>
+    extends _${{feature_name.pascalCase()}}StateCopyWithImpl<$Res, _${{feature_name.pascalCase()}}StateImpl>
+    implements _$${{feature_name.pascalCase()}}StateImplCopyWith<$Res> {
+  __$${{feature_name.pascalCase()}}StateImplCopyWithImpl(
+      _${{feature_name.pascalCase()}}StateImpl _value, $Res Function(_${{feature_name.pascalCase()}}StateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of {{feature_name.pascalCase()}}State
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _${{feature_name.pascalCase()}}StateImpl extends _{{feature_name.pascalCase()}}State {
+  const _${{feature_name.pascalCase()}}StateImpl() : super._();
+
+  @override
+  String toString() {
+    return '{{feature_name.pascalCase()}}State()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _${{feature_name.pascalCase()}}StateImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function() $default,
+  ) {
+    return $default();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function()? $default,
+  ) {
+    return $default?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function()? $default, {
+    required TResult orElse(),
+  }) {
+    if ($default != null) {
+      return $default();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(_{{feature_name.pascalCase()}}State value) $default,
+  ) {
+    return $default(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(_{{feature_name.pascalCase()}}State value)? $default,
+  ) {
+    return $default?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(_{{feature_name.pascalCase()}}State value)? $default, {
+    required TResult orElse(),
+  }) {
+    if ($default != null) {
+      return $default(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _{{feature_name.pascalCase()}}State extends {{feature_name.pascalCase()}}State {
+  const factory _{{feature_name.pascalCase()}}State() = _${{feature_name.pascalCase()}}StateImpl;
+  const _{{feature_name.pascalCase()}}State._() : super._();
+}
